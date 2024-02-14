3738a2cb1bdb365d665fad7582e0b11cec7b18e4
Alex-Horan <alexandermhoran@gmail.com>
2024-02-14 12:55:35 -0500

    removed unnecessary code from previous project version using dart/flutter

--- FlaskServer/sentParsing.py
+++ FlaskServer/sentParsing.py
@@ -7,11 +7,14 @@ from flask import jsonify
 import shutil
 import os
 
+config = imgkit.config(wkhtmltoimage="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltoimage.exe")
+
+nlp = spacy.load("en_core_web_sm") # loads spacy pipeline
 language = Language.English #sets the language for con tree
 spacy_model_size = ConstituentTree.SpacyModelSize.Small
 conlp = ConstituentTree.create_pipeline(language, spacy_model_size) #creates pipeline
 
-nlp = spacy.load("en_core_web_sm")
+
 
 
 
@@ -21,7 +24,7 @@ def depTree(sentence: str): #generates dependency tree
     if sentence is not None:
         doc = nlp(sentence)
         html = displacy.render(doc, style="dep")
-        imgkit.from_string(html, 'depTree.png')
+        imgkit.from_string(html, 'depTree.png', config=config)
         
         
     
@@ -31,6 +34,7 @@ def conTree(sentence: str): #generates constituent tree
         tree.export_tree(destination_filepath='conTree.png', verbose=True)
         
 
-async def graphGen(sentence: str):
-    await depTree(sentence)
-    await conTree(sentence)
+def graphGen(sentence: str):
+    depTree(sentence)
+    conTree(sentence)
+    return "done"
