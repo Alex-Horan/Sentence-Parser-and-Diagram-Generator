3717d00124f81c665f4cdda5309e2ce9b0d86c4b
Alex-Horan <alexandermhoran@gmail.com>
2024-02-14 12:56:20 -0500

    removed/changed outdated code from previous project version that used flutter/dart

--- FlaskServer/views.py
+++ FlaskServer/views.py
@@ -5,20 +5,17 @@ from . import sentParsing
 import os
 import os.path
 
-bp = Blueprint('graph', __name__, url_prefix='/')
+bp = Blueprint('graph', __name__, url_prefix='/') #helps separate views from the init file
 
-@bp.route('/', methods=('GET', 'POST'))
+@bp.route('/', methods=('POST')) #routing the default page
 def index():
-    if (request.method == 'GET'):
-        return render_template('base.html', type=request.args.get('q'))
-
     if (request.method == 'POST') and request.is_json:
         
         print("received post request")
         response = request.get_json()
-        
-        result = sentParsing.graphGen(response['body'])
+        print(response)
+        result = sentParsing.graphGen(response)
         if result:
-            return render_template('base.html')
+            return result
         else:
             return jsonify("<h1>Error creating dependency tree</h1>")
