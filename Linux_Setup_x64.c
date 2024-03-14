7923066fa5f75d957a04a0528f5823f2e35bb1d0
Alex-Horan <alexandermhoran@gmail.com>
2024-03-14 11:41:00 -0400

    removed unnecessary code

--- Linux_Setup_x64.c
+++ Linux_Setup_x64.c
@@ -4,9 +4,5 @@
 int main() {
     //downloads the setup python script
     system("curl https://raw.githubusercontent.com/Alex-Horan/Sentence-Parser-and-Diagram-Generator/master/setup_files/Installer_linux_x64.py -o Installer_linux_x64.py; python3 ./Installer_linux_x64.py");
-    //runs the setup
-    
-    //testing
-    system("echo 'testing\n'; echo 'testing 2'");
     return 0;
 }
