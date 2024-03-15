3520880d8e11c1fbf25daa644d3f61974ab920a1
Alex-Horan <alexandermhoran@gmail.com>
2024-03-15 10:14:16 -0400

    Testing first idea for how to run both the electron app and flask server at the same time

--- startAppLinux.py
+++ startAppLinux.py
@@ -3,13 +3,20 @@ import os
 
 def startApp():
     result = subprocess.run(["../App/bin/python3 -m flask --app FlaskServer run;"], shell=True, capture_output=True)
-    if result.returncode != 0:
-        print("Failed to start Flask Server")
+    if result.returncode is None:
+        print("flask server is running and is executing")
+        pass
+        # print("Failed to start Flask Server")
+        # input("Press <Enter> to exit...")
+        # exit()
+    elif result.returncode != 0:
+        print("Flask server failed to start")
         input("Press <Enter> to exit...")
         exit()
     else:
-        print("testing")
-        pass
+        print("flask server has closed")
+        input("Press <Enter> to exit...")
+        exit()
     # print("Flask server has started")
     # appStart = subprocess.run(["npm start"], shell=True, capture_output=True)
     # if appStart.returncode != 0:
