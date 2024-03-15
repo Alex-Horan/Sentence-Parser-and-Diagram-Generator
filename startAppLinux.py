f72d95c70dbb2d9ed459382753955a95136d0fac
Alex-Horan <alexandermhoran@gmail.com>
2024-03-15 10:26:36 -0400

    Trying another method of running flask server in background before resulting to external packages

--- startAppLinux.py
+++ startAppLinux.py
@@ -2,28 +2,21 @@ import subprocess
 import os
 
 def startApp():
-    result = subprocess.run(["../App/bin/python3 -m flask --app FlaskServer run;"], shell=True, capture_output=True)
-    if result.returncode is None:
-        print("flask server is running and is executing")
-        pass
-        # print("Failed to start Flask Server")
-        # input("Press <Enter> to exit...")
-        # exit()
-    elif result.returncode != 0:
-        print("Flask server failed to start")
+    result = subprocess.run(["../App/bin/python3 -m flask --app FlaskServer run &"], shell=True, capture_output=True)
+    if result.returncode != 0:
+        print("Failed to start Flask Server")
         input("Press <Enter> to exit...")
         exit()
     else:
-        print("flask server has closed")
+        print("yoop")
+        pass
+    print("Flask server has started")
+    appStart = subprocess.run(["npm start"], shell=True, capture_output=True)
+    if appStart.returncode != 0:
+        print("Failed to start Electron app.")
         input("Press <Enter> to exit...")
         exit()
-    # print("Flask server has started")
-    # appStart = subprocess.run(["npm start"], shell=True, capture_output=True)
-    # if appStart.returncode != 0:
-    #     print("Failed to start Electron app.")
-    #     input("Press <Enter> to exit...")
-    #     exit()
-    # else:
-    #     pass
+    else:
+        pass
 
 startApp()
