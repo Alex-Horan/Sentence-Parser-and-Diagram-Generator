593bc21498abb427ea28d3500c0011c7761e7ec5
Alex-Horan <alexandermhoran@gmail.com>
2024-03-21 14:37:31 -0400

    turns out github is just being weird

--- Windows_Setup_x64.c
+++ Windows_Setup_x64.c
@@ -3,7 +3,7 @@
 
 int main() {
     //downloads the setup python script
-    system("curl https://raw.githubusercontent.com/Alex-Horan/Sentence-Parser-and-Diagram-Generator/master/setup_files/Installer_windows_x64.py -o Installer_Windows_x64.py");
+    system("curl https://raw.githubusercontent.com/Alex-Horan/Sentence-Parser-and-Diagram-Generator/master/setup_files/installer_windows_x64.py -o Installer_Windows_x64.py");
     system("py ./Installer_Windows_x64.py");
     return 0;
 }
